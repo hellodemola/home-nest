312afc66f6b08e1cebb66fa452f58db0de53769f
Ademola Onasoga <ademola.onasoga@gmail.com>
2024-11-06 12:29:23 +0100

    add trime to value

--- src/common/dto/user/index.dto.ts
+++ src/common/dto/user/index.dto.ts
@@ -1,16 +1,20 @@
 import { IsEmail, IsPhoneNumber, Length } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateUserDto {
   @IsEmail()
+  @Transform(({ value }) => value.trim())
   email: string;
 
   @Length(6, 18)
   password: string;
 
   @Length(3, 18)
+  @Transform(({ value }) => value.trim())
   firstName: string;
 
   @Length(3, 18)
+  @Transform(({ value }) => value.trim())
   lastName: string;
 
   @IsPhoneNumber()
@@ -19,9 +23,11 @@ export class CreateUserDto {
 
 export class UpdateUserDto {
   @Length(3, 18)
+  @Transform(({ value }) => value.trim())
   firstName: string;
 
   @Length(3, 18)
+  @Transform(({ value }) => value.trim())
   lastName: string;
 
   @IsPhoneNumber()
